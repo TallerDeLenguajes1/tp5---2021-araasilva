425c32e210006edb0f00f4e3a853aa3cfcb5dabb
Araceli <araasilva96@gmail.com>
2021-05-19 23:57:27 -0300

    Punto 1

--- TrabajoPractico5/TrabajoPractico5/Program.cs
+++ TrabajoPractico5/TrabajoPractico5/Program.cs
@@ -4,31 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrabajoPractico5
+namespace PrimerPunto
 {
     class Punto1
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Ingrese un numero");
             int num = Convert.ToInt32(Console.ReadLine());
-            
+
             if (num > 0)
             {
                 int inverso = 0, aux;
                 while (num > 0)
                 {
                     aux = num % 10;
-                    num = num / 10;
+                    num /= 10;
                     inverso = inverso * 10 + aux;
                 }
                 Console.WriteLine("El numero invertido es: " + inverso);
-            }else Console.WriteLine("el numero ingresado es menor o igual que 0");
-            
+            }
+            else Console.WriteLine("el numero ingresado es menor o igual que 0");
 
         }
-        
-       
     }
-
 }
